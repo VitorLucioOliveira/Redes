d29d801d7623d6ad0205b55b41bb6cd2d9da7a5f
VitorLucioOliveira <vitor.lucio.0916@gmail.com>
2025-04-22 16:29:45 -0300

    local host

--- udp/Cliente.java
+++ udp/Cliente.java
@@ -1,5 +1,8 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 class Cliente
 {
@@ -8,6 +11,7 @@ class Cliente
    private static byte[] receiveData = new byte[1024];
 
    public static byte[] lerString () throws Exception {
+      System.out.println("Digite mensagem");
       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
       return in.readLine().getBytes();
    }
@@ -15,7 +19,7 @@ class Cliente
    public static void main(String args[]) throws Exception
    {
       DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress ipServidor = InetAddress.getByName("192.168.84.220");
+      InetAddress ipServidor = InetAddress.getByName("localhost");
       sendData = lerString();
 
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipServidor, portaServidor);
